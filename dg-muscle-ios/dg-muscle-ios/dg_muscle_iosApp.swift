82d0e240d8bd557f6486edc5ec30eefde49201eb
신동규 <donggyu9410@gmail.com>
2023-09-23 20:18:54 +0900

    Project init

---
+++ dg-muscle-ios/dg-muscle-ios/dg_muscle_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  dg_muscle_iosApp.swift
+//  dg-muscle-ios
+//
+//  Created by 신동규 on 2023/09/23.
+//
+
+import SwiftUI
+
+@main
+struct dg_muscle_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
