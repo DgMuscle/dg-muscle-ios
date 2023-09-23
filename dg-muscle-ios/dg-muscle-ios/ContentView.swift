f1dba98b005d29132634553b71217fb8bf7d3435
신동규 <donggyu9410@gmail.com>
2023-09-23 21:47:42 +0900

    SignInView

--- dg-muscle-ios/dg-muscle-ios/ContentView.swift
+++ dg-muscle-ios/dg-muscle-ios/ContentView.swift
@@ -6,9 +6,27 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 
 struct ContentView: View {
+    
+    @State private var login = false
+    
+    init() {
+        Auth.auth().addStateDidChangeListener { [self] _, user in
+            withAnimation {
+                self.login = user != nil
+            }
+        }
+    }
+    
     var body: some View {
-        SignInView()
+        ZStack {
+            if login {
+                SignInView()
+            } else {
+                Text("logged in")
+            }
+        }
     }
 }
