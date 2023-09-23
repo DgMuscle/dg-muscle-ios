bec7cba8046f52dcda6ad5cdf5ae20ff9b44f739
신동규 <donggyu9410@gmail.com>
2023-09-23 21:43:57 +0900

    SignInView

--- dg-muscle-ios/dg-muscle-ios/ContentView.swift
+++ dg-muscle-ios/dg-muscle-ios/ContentView.swift
@@ -9,16 +9,6 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        SignInView()
     }
-}
-
-#Preview {
-    ContentView()
 }
