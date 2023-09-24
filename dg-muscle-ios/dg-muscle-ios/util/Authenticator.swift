c482f401a8d26d0b6d664c96eb1c24dc6d6b2b15
신동규 <donggyu9410@gmail.com>
2023-09-24 16:19:17 +0900

    UpdateUser

--- dg-muscle-ios/dg-muscle-ios/util/Authenticator.swift
+++ dg-muscle-ios/dg-muscle-ios/util/Authenticator.swift
@@ -5,10 +5,18 @@
 //  Created by 신동규 on 2023/09/23.
 //
 
+import Foundation
 import FirebaseAuth
 
 final class Authenticator {
     func signOut() throws {
         try Auth.auth().signOut()
+    }
+    
+    func updateUser(displayName: String?, photoURL: URL?) async throws {
+        let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+        changeRequest?.displayName = displayName
+        changeRequest?.photoURL = photoURL
+        try await changeRequest?.commitChanges()
     }
 }
