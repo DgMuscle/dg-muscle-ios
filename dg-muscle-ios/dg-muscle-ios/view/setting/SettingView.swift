58646052cb0a33052035bbbdac86bbf089d9fbfc
신동규 <donggyu9410@gmail.com>
2023-09-28 21:22:29 +0900

    apply settingview

--- dg-muscle-ios/dg-muscle-ios/view/setting/SettingView.swift
+++ dg-muscle-ios/dg-muscle-ios/view/setting/SettingView.swift
@@ -12,28 +12,19 @@ protocol SettingViewDependency {
     func tapDisplayName()
     func tapProfileImage()
     func error(error: Error)
-    
     func signOut() throws
 }
 
 struct SettingView: View {
     let dependency: SettingViewDependency
     
+    @State var isShowingProfileBoxView = false
     @StateObject private var userStore: UserStore = store.user
     private let profileImageSize: CGFloat = 30
     
     var body: some View {
-        VStack(alignment: .leading) {
-            
-            Menu {
-                Button("display name", systemImage: "pencil") {
-                    dependency.tapDisplayName()
-                }
-                
-                Button("profile image", systemImage: "person") {
-                    dependency.tapProfileImage()
-                }
-            } label: {
+        ZStack {
+            VStack(alignment: .leading) {
                 HStack {
                     KFImage(userStore.photoURL)
                         .placeholder {
@@ -55,20 +46,30 @@ struct SettingView: View {
                     RoundedRectangle(cornerRadius: 8).fill(Color(uiColor: .systemBackground).gradient)
                         .shadow(radius: 0.1)
                 }
-            }
-
-            Button("sign out") {
-                do {
-                    try dependency.signOut()
-                } catch {
-                    dependency.error(error: error)
+                .onTapGesture {
+                    Vibration.soft.vibrate()
+                    withAnimation {
+                        isShowingProfileBoxView.toggle()
+                    }
                 }
+
+                Button("sign out") {
+                    do {
+                        try dependency.signOut()
+                    } catch {
+                        dependency.error(error: error)
+                    }
+                }
+                .padding(.top, 20)
+                .foregroundStyle(.red)
+                
+                Spacer().frame(maxWidth: .infinity)
             }
-            .padding(.top, 20)
-            .foregroundStyle(.red)
+            .padding()
             
-            Spacer().frame(maxWidth: .infinity)
+            if isShowingProfileBoxView {
+                ProfileBoxView(dependency: dependency, isShowing: $isShowingProfileBoxView)
+            }
         }
-        .padding()
     }
 }
