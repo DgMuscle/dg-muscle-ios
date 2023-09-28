40c8bab68d9c78178b7a8b71699e99b8521c20cb
신동규 <donggyu9410@gmail.com>
2023-09-28 11:33:06 +0900

    PhotoPickerView

---
+++ dg-muscle-ios/dg-muscle-ios/view/global/PhotoPickerView.swift
@@ -0,0 +1,62 @@
+//
+//  PhotoPickerView.swift
+//  dg-muscle-ios
+//
+//  Created by 신동규 on 2023/09/28.
+//
+
+import SwiftUI
+import PhotosUI
+
+protocol PhotoPickerViewDependency {
+    func savePhoto(image: UIImage?)
+}
+
+struct PhotoPickerView: View {
+    
+    @State private var selectedItem: PhotosPickerItem?
+    @State var uiImage: UIImage?
+    @Binding var isShowing: Bool
+    
+    let dependency: PhotoPickerViewDependency
+    
+    var body: some View {
+        ZStack {
+            PhotosPicker(
+                selection: $selectedItem,
+                matching: .images,
+                photoLibrary: .shared()
+            ) {
+                if let uiImage {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    VStack(spacing: 8) {
+                        Image(systemName: "plus.rectangle")
+                            .font(.largeTitle)
+                        Text("Select a photo")
+                    }
+                }
+            }
+            
+            VStack {
+                Spacer()
+                Button("Save") {
+                    dependency.savePhoto(image: uiImage)
+                    withAnimation {
+                        isShowing.toggle()
+                    }
+                }
+            }
+        }
+        .onChange(of: selectedItem) { _, newValue in
+            Task {
+                guard let data = try? await newValue?.loadTransferable(type: Data.self) else { return }
+                withAnimation {
+                    uiImage = UIImage(data: data)
+                }
+            }
+        }
+    }
+}
