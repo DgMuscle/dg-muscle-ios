6899c8817538e9a14989857b2425d3e4cc3c5629
신동규 <donggyu9410@gmail.com>
2023-09-30 20:52:32 +0900

    ExerciseStore

---
+++ dg-muscle-ios/dg-muscle-ios/store/ExerciseStore.swift
@@ -0,0 +1,26 @@
+//
+//  ExerciseStore.swift
+//  dg-muscle-ios
+//
+//  Created by 신동규 on 9/30/23.
+//
+
+import Combine
+import Foundation
+
+final class ExerciseStore {
+    static let shared = ExerciseStore()
+    
+    @Published private(set) var exercises: [Exercise] = []
+    
+    private init() { }
+    
+    func updateExercises() {
+        Task {
+            let exercises = try await ExerciseRepository.shared.get()
+            DispatchQueue.main.async {
+                self.exercises = exercises
+            }
+        }
+    }
+}
