d3db83ffe1b67ed8ad41da2f59ec3405cfdad2ed
donggyu <donggyu_00@naver.com>
2023-11-25 20:44:02 +0900

    improve photo picker view (#53)

--- dg-muscle-ios/dg-muscle-ios/view/global/PhotoPickerView.swift
+++ dg-muscle-ios/dg-muscle-ios/view/global/PhotoPickerView.swift
@@ -15,66 +15,76 @@ protocol PhotoPickerViewDependency {
 struct PhotoPickerView: View {
     
     @State private var selectedItem: PhotosPickerItem?
+    @State private var localShowing = true
+    
     @State var uiImage: UIImage?
+    
     @Binding var isShowing: Bool
     
     let dependency: PhotoPickerViewDependency
-    private let buttonSize: CGFloat = 35
     
     var body: some View {
         ZStack {
-            
-            Rectangle()
-                .fill(Color(uiColor: .systemBackground)).ignoresSafeArea()
-            
-            PhotosPicker(
-                selection: $selectedItem,
-                matching: .images,
-                photoLibrary: .shared()
-            ) {
-                if let uiImage {
-                    Image(uiImage: uiImage)
-                        .resizable()
-                        .scaledToFit()
-                } else {
-                    Text("Select a photo")
-                }
-            }
-            
-            VStack {
-                HStack(spacing: 30) {
-                    Spacer()
-                    Button {
-                        withAnimation {
-                            isShowing = false
-                        }
-                    } label: {
-                        Image(systemName: "xmark")
-                            .foregroundStyle(.white)
-                            .background {
-                                Circle().fill(.gray.opacity(0.6))
-                                    .frame(width: buttonSize, height: buttonSize)
-                            }
+            if localShowing {
+                Rectangle()
+                    .fill(Color(uiColor: .systemBackground)).ignoresSafeArea()
+                
+                PhotosPicker(
+                    selection: $selectedItem,
+                    matching: .images,
+                    photoLibrary: .shared()
+                ) {
+                    if let uiImage {
+                        Image(uiImage: uiImage)
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        Text("Select a photo")
                     }
+                }
+                
+                VStack {
+                    HStack(spacing: 15) {
+                        Spacer()
+                        
+                        if uiImage != nil {
+                            Button("delete photo") {
+                                withAnimation {
+                                    uiImage = nil
+                                }
+                            }
+                            .foregroundStyle(.red)
+                            .buttonBorderShape(.buttonBorder)
+                        }
+                        
+                        Button {
+                            withAnimation {
+                                localShowing = false
+                            }
+                        } label: {
+                            Image(systemName: "xmark")
+                                .foregroundStyle(.white)
+                        }
+                        .buttonStyle(.bordered)
+                    }
+                    .padding()
+                    Spacer()
                     
-                    Button {
+                    Button("save photo") {
                         dependency.saveProfileImage(image: uiImage)
                         withAnimation {
-                            isShowing = false
+                            localShowing = false
                         }
-                    } label: {
-                        Image(systemName: "square.and.arrow.down")
-                            .foregroundStyle(.white)
-                            .background {
-                                Circle().fill(.gray.opacity(0.6))
-                                    .frame(width: buttonSize, height: buttonSize)
-                            }
                     }
+                    .buttonStyle(.borderedProminent)
                 }
-                .padding()
-                Spacer()
             }
         }
+        .onChange(of: localShowing, { oldValue, newValue in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                isShowing = false
+            }
+        })
         .onChange(of: selectedItem) { _, newValue in
             Task {
                 guard let data = try? await newValue?.loadTransferable(type: Data.self) else { return }
