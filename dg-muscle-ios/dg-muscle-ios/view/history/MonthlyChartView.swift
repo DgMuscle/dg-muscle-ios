1310bd5ea995ac9a8cd0cbfca8bf8f21e9d92660
donggyu <donggyu_00@naver.com>
2023-11-25 23:02:21 +0900

    Monthlychartview dismiss animation (#57)
    * add dismiss animation
    
    * change showing changing duration

--- dg-muscle-ios/dg-muscle-ios/view/history/MonthlyChartView.swift
+++ dg-muscle-ios/dg-muscle-ios/view/history/MonthlyChartView.swift
@@ -12,6 +12,7 @@ struct MonthlyChartView: View {
     let histories: [ExerciseHistory]
     let volumeByPart: [String: Double]
     @State var markType: ChartView.MarkType = .bar
+    @State var localShowing = true
     @Binding var showing: Bool
     
     private let allExercises: [Exercise]
@@ -31,71 +32,80 @@ struct MonthlyChartView: View {
     }
     
     var body: some View {
-        VStack {
-            HStack {
-                Picker("mark type", selection: $markType) {
-                    ForEach(ChartView.MarkType.allCases, id: \.self) {
-                        Text($0.rawValue)
-                    }
-                }
-                .pickerStyle(.segmented)
-                
-                Button {
-                    withAnimation {
-                        showing = false
-                    }
-                } label: {
-                    Image(systemName: "xmark")
-                        .foregroundStyle(.white)
-                        .padding(6)
-                        .background {
-                            Circle().fill(Color.gray.opacity(0.6))
-                        }
-                }
-            }
-            .padding()
-            
-            ScrollView {
+        ZStack {
+            if localShowing {
                 VStack {
-                    ForEach(allExercises, id: \.self) { exercise in
-                        
-                        HStack {
-                            Text(exercise.name).font(.title2.bold())
-                            Spacer()
-                        }
-                        
-                        HStack {
-                            ForEach(exercise.parts, id: \.self) { part in
-                                Text(part.rawValue)
-                                    .foregroundStyle(.white)
-                                    .font(.caption2)
-                                    .padding(.vertical, 4)
-                                    .padding(.horizontal, 8)
-                                    .background {
-                                        Capsule().fill(Color.blue).opacity(0.5)
-                                    }
+                    HStack {
+                        Picker("mark type", selection: $markType) {
+                            ForEach(ChartView.MarkType.allCases, id: \.self) {
+                                Text($0.rawValue)
                             }
-                            Spacer()
                         }
+                        .pickerStyle(.segmented)
                         
-                        ChartView(datas: ChartView.generateDataBasedOnExercise(from: histories, exerciseId: exercise.id, length: 7),
-                                  markType: $markType,
-                                  valueName: "volume",
-                                  additionalMax: 1500
-                        )
-                        .frame(height: 235)
-                        
-                        Spacer(minLength: 50)
+                        Button {
+                            withAnimation {
+                                localShowing = false
+                            }
+                        } label: {
+                            Image(systemName: "xmark")
+                                .foregroundStyle(.white)
+                                .padding(6)
+                                .background {
+                                    Circle().fill(Color.gray.opacity(0.6))
+                                }
+                        }
                     }
+                    .padding()
                     
+                    ScrollView {
+                        VStack {
+                            ForEach(allExercises, id: \.self) { exercise in
+                                
+                                HStack {
+                                    Text(exercise.name).font(.title2.bold())
+                                    Spacer()
+                                }
+                                
+                                HStack {
+                                    ForEach(exercise.parts, id: \.self) { part in
+                                        Text(part.rawValue)
+                                            .foregroundStyle(.white)
+                                            .font(.caption2)
+                                            .padding(.vertical, 4)
+                                            .padding(.horizontal, 8)
+                                            .background {
+                                                Capsule().fill(Color.blue).opacity(0.5)
+                                            }
+                                    }
+                                    Spacer()
+                                }
+                                
+                                ChartView(datas: ChartView.generateDataBasedOnExercise(from: histories, exerciseId: exercise.id, length: 7),
+                                          markType: $markType,
+                                          valueName: "volume",
+                                          additionalMax: 1500
+                                )
+                                .frame(height: 235)
+                                
+                                Spacer(minLength: 50)
+                            }
+                            
+                        }
+                        .padding()
+                    }
+                    .scrollIndicators(.hidden)
                 }
-                .padding()
+                .background {
+                    Rectangle().fill(Color(uiColor: .systemBackground))
+                        .ignoresSafeArea()
+                }
             }
-            .scrollIndicators(.hidden)
         }
-        .background {
-            Rectangle().fill(Color(uiColor: .systemBackground))
-                .ignoresSafeArea()
+        .onChange(of: localShowing) { oldValue, newValue in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                showing = newValue
+            }
         }
     }
 }
