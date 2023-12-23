09044b762221c17fd963079bdb27a511ab0a4924
신동규 <donggyu9410@gmail.com>
2023-12-23 17:25:13 +0900

    add grass view

--- dg-muscle-ios/dg-muscle-ios-widget/dg_muscle_ios_widget.swift
+++ dg-muscle-ios/dg-muscle-ios-widget/dg_muscle_ios_widget.swift
@@ -41,13 +41,7 @@ struct dg_muscle_ios_widgetEntryView : View {
     var entry: Provider.Entry
 
     var body: some View {
-        VStack {
-            Text("Time:")
-            Text(entry.date, style: .time)
-
-            Text("Favorite Emoji:")
-            Text(entry.configuration.favoriteEmoji)
-        }
+        GrassView()
     }
 }
 
---
+++ dg-muscle-ios/dg-muscle-ios-widget/view/GrassView.swift
@@ -0,0 +1,14 @@
+//
+//  GrassView.swift
+//  dg-muscle-ios
+//
+//  Created by 신동규 on 12/23/23.
+//
+
+import SwiftUI
+
+struct GrassView: View {
+    var body: some View {
+        Text("GrassView")
+    }
+}
