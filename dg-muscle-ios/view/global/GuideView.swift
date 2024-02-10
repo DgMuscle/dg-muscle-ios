f46abcb7e2e1f75a6da7e5c2bcbe8d97968d2686
신동규 <donggyu9410@gmail.com>
2024-02-10 14:58:15 +0900

    add guide view

---
+++ dg-muscle-ios/view/global/GuideView.swift
@@ -0,0 +1,28 @@
+//
+//  GuideView.swift
+//  dg-muscle-ios
+//
+//  Created by 신동규 on 2/10/24.
+//
+
+import SwiftUI
+
+struct GuideView: View {
+    @State var loading = false
+    @State var error: Error?
+    
+    var body: some View {
+        ZStack {
+            if let url = URL(string: "https://judicious-hoof-33e.notion.site/dg-muscle-app-guide-2f58d99c886b458eaca6b82f1403fa4b") {
+                WebView(url: url, loading: $loading, error: $error)
+                    .ignoresSafeArea()
+            } else {
+                Text("Fail to load.")
+            }
+        }
+    }
+}
+
+#Preview {
+    GuideView().preferredColorScheme(.dark)
+}
