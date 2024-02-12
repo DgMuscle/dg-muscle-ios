d0d7d8758683d4cd275498bb13321c0440732daa
donggyu <donggyu_00@naver.com>
2024-02-12 17:42:10 +0900

    Localizing (#99)
    * localizing guideview
    
    * localizing introduceview

--- dg-muscle-ios/view/setting/etc section/IntroduceView.swift
+++ dg-muscle-ios/view/setting/etc section/IntroduceView.swift
@@ -11,9 +11,23 @@ struct IntroduceView: View {
     @State var loading = false
     @State var error: Error?
     
+    var isKorean: Bool {
+        let preferredLanguages = UserDefaults.standard.object(forKey: "AppleLanguages") as? [String]
+        let isKorean = preferredLanguages?.first == "ko"
+        return isKorean
+    }
+    
+    var url: String {
+        if isKorean {
+            return "https://judicious-hoof-33e.notion.site/dgmuscle-ios-ko-a40c39718610412599b9d6ee2f0ad5a4"
+        } else {
+            return "https://judicious-hoof-33e.notion.site/dgmuscle-ios-a7162152c1594a09902d7d6c07da8bdd"
+        }
+    }
+    
     var body: some View {
         ZStack {
-            if let url = URL(string: "https://judicious-hoof-33e.notion.site/dgmuscle-ios-a7162152c1594a09902d7d6c07da8bdd") {
+            if let url = URL(string: url) {
                 WebView(url: url, loading: $loading, error: $error)
             } else {
                 Text("Fail to load.")
