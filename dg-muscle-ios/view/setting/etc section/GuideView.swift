9e3d336d44a6769d41ba4243764be4b72066db38
신동규 <donggyu9410@gmail.com>
2024-02-12 17:26:38 +0900

    localizing guideview

--- dg-muscle-ios/view/setting/etc section/GuideView.swift
+++ dg-muscle-ios/view/setting/etc section/GuideView.swift
@@ -11,9 +11,23 @@ struct GuideView: View {
     @State var loading = false
     @State var error: Error?
     
+    var isKorean: Bool {
+        let preferredLanguages = UserDefaults.standard.object(forKey: "AppleLanguages") as? [String]
+        let isKorean = preferredLanguages?.first == "ko"
+        return isKorean
+    }
+    
+    var url: String {
+        if isKorean {
+            return "https://judicious-hoof-33e.notion.site/dgmuscle-app-guide-ko-51d0cb09dd9d42cba9fe6d8ad1e2aa79"
+        } else {
+            return "https://judicious-hoof-33e.notion.site/dg-muscle-app-guide-2f58d99c886b458eaca6b82f1403fa4b"
+        }
+    }
+    
     var body: some View {
         ZStack {
-            if let url = URL(string: "https://judicious-hoof-33e.notion.site/dg-muscle-app-guide-2f58d99c886b458eaca6b82f1403fa4b") {
+            if let url = URL(string: url) {
                 WebView(url: url, loading: $loading, error: $error)
             } else {
                 Text("Fail to load.")
