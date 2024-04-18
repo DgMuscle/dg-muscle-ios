0fb43b638475c3bd6fb643f8194c4ab84987466d
신동규 <donggyu9410@gmail.com>
2024-04-18 22:22:18 +0900

    RemoveAccountView

---
+++ dg-muscle-ios/V2/app/setting/components/removeAccount/RemoveAccountViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  RemoveAccountViewModel.swift
+//  dg-muscle-ios
+//
+//  Created by 신동규 on 4/18/24.
+//
+
+import Foundation
+import Combine
+import SwiftUI
+
+final class RemoveAccountViewModel: ObservableObject {
+    
+    @Published var displayName: String = ""
+    @Published var localErrorMessage: String?
+    
+    let userRepository: UserRepositoryV2
+    @Binding var loading: Bool
+    @Binding var errorMessage: String?
+    @Binding var isPresent: Bool
+    
+    init(userRepository: UserRepositoryV2,
+         loading: Binding<Bool>,
+         errorMessage: Binding<String?>,
+         isPresent: Binding<Bool>) {
+        self.userRepository = userRepository
+        self._loading = loading
+        self._errorMessage = errorMessage
+        self._isPresent = isPresent
+    }
+    
+    @MainActor
+    func tapRemoveButton() {
+        Task {
+            guard loading == false else { return }
+            
+            guard userRepository.user?.displayName ?? "" == self.displayName else {
+                localErrorMessage = "Please enter your display name again."
+                return
+            }
+            
+            loading = true
+            isPresent.toggle()
+            let error = await userRepository.withDrawal()
+            self.errorMessage = error?.localizedDescription
+            loading = false
+        }
+    }
+}
