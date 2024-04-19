a3eb833b69302713681623bf36bb6ee7a386699e
donggyushin <donggyu9410@gmail.com>
2024-04-19 17:04:59 +0900

    prepare preview

--- dg-muscle-ios/V2/app/history/screen/monthlySection/MonthlySectionView.swift
+++ dg-muscle-ios/V2/app/history/screen/monthlySection/MonthlySectionView.swift
@@ -8,11 +8,22 @@
 import SwiftUI
 
 struct MonthlySectionView: View {
+    
+    @StateObject var viewModel: MonthlySectionViewModel
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    MonthlySectionView()
+    
+    let histories: [ExerciseHistorySection.History] =
+    HistoryRepositoryV2Test().histories.map({ .init(exercise: $0, metadata: nil) })
+    let sectionData = ExerciseHistorySection(histories: histories)
+    
+    let viewModel = MonthlySectionViewModel(exerciseHistorySection: sectionData,
+                                            exerciseRepository: ExerciseRepositoryV2Test())
+    
+    return MonthlySectionView(viewModel: viewModel).preferredColorScheme(.dark)
 }
