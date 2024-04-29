4b63340bc3a608d68ae0248af51bc5a68ade4bfa
donggyushin <donggyu9410@gmail.com>
2024-04-29 11:43:12 +0900

    feat: HistoryFormSetsViewModel

--- dg-muscle-ios/presenter/view/history/form/HistoryFormSetsViewModel.swift
+++ dg-muscle-ios/presenter/view/history/form/HistoryFormSetsViewModel.swift
@@ -20,15 +20,23 @@ final class HistoryFormSetsViewModel: ObservableObject {
     @Published var previousRecordVolume: Double? = nil
     
     private let previousDateString: String
+    private let getPreviousRecordUsecase: GetPreviousRecordUsecase
     private var cancellables = Set<AnyCancellable>()
     
     init(record: Binding<RecordV>,
-         previousDateString: String) {
+         previousDateString: String,
+         getPreviousRecordUsecase: GetPreviousRecordUsecase) {
         _record = record
         self.previousDateString = previousDateString
+        self.getPreviousRecordUsecase = getPreviousRecordUsecase
         self.sets = self.record.sets
         
         bind()
+        configurePreviousRecord(record: record.wrappedValue)
+    }
+    
+    func post(set: ExerciseSetV) {
+        sets.append(set)
     }
     
     private func bind() {
@@ -40,5 +48,21 @@ final class HistoryFormSetsViewModel: ObservableObject {
                 self?.record.sets = sets
             }
             .store(in: &cancellables)
+        
+        $previousRecord
+            .receive(on: DispatchQueue.main)
+            .compactMap({ $0 })
+            .sink { [weak self] record in
+                self?.previousRecordVolume = record.volume
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func configurePreviousRecord(record: RecordV) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMMdd"
+        guard let date = dateFormatter.date(from: previousDateString) else { return }
+        guard let previousRecordDomain = getPreviousRecordUsecase.implement(record: record.domain, date: date) else { return }
+        previousRecord = .init(from: previousRecordDomain)
     }
 }
