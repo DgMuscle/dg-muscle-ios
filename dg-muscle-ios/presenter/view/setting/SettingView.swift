08d964d1fa3831ca9844d6941b70645c61c2ae78
신동규 <donggyu9410@gmail.com>
2024-04-30 11:26:09 +0900

    setting view ui

--- dg-muscle-ios/presenter/view/setting/SettingView.swift
+++ dg-muscle-ios/presenter/view/setting/SettingView.swift
@@ -5,4 +5,58 @@
 //  Created by 신동규 on 4/30/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct SettingView: View {
+    
+    @StateObject var viewModel: SettingViewModel
+    @EnvironmentObject var coordinator: CoordinatorV2
+    
+    var body: some View {
+        List {
+            Section {
+                VStack {
+                    Button {
+                        coordinator.main.openWeb(url: "https://judicious-hoof-33e.notion.site/dgmuscle-ios-a7162152c1594a09902d7d6c07da8bdd")
+                    } label: {
+                        ListItemView(systemImageName: "a.book.closed",
+                                     title: "DgMuscle Introduce",
+                                     color: .purple)
+                    }
+                    
+                    Button {
+                        coordinator.exercise.manage()
+                    } label: {
+                        ListItemView(systemImageName: "dumbbell.fill",
+                                     title: "Exercise",
+                                     description: "Manage your exercise list",
+                                     color: .blue)
+                    }
+                }
+            } header: {
+                VStack {
+                    if let user = viewModel.user {
+                        UserBoxView2(user: user, descriptionLabel: "Configure Your Profile")
+                            .padding(.bottom)
+                    }
+                }
+            } footer: {
+                VStack(spacing: 20) {
+                    ListItemView(systemImageName: "figure.run", title: "LOGOUT", color: .green)
+                    ListItemView(systemImageName: "trash", title: "DELETE ACCOUNT", color: .red)
+                }
+                .padding(.top, 40)
+            }
+        }
+        .navigationTitle("Setting")
+        .navigationBarTitleDisplayMode(.large)
+    }
+}
+
+#Preview {
+    let viewModel: SettingViewModel = .init(subscribeUserUsecase: 
+            .init(userRepository: UserRepositoryTest()))
+    return SettingView(viewModel: viewModel)
+        .preferredColorScheme(.dark)
+        .environmentObject(CoordinatorV2(path: .constant(.init())))
+}
