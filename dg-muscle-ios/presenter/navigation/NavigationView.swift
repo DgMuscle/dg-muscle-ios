b05703841b9d0c99419aabeca6511c1350d506c8
신동규 <donggyu9410@gmail.com>
2024-04-30 17:46:48 +0900

    feat: navigation view

---
+++ dg-muscle-ios/presenter/navigation/NavigationView.swift
@@ -0,0 +1,14 @@
+//
+//  NavigationView.swift
+//  dg-muscle-ios
+//
+//  Created by 신동규 on 4/30/24.
+//
+
+import SwiftUI
+
+struct NavigationView: View {
+    var body: some View {
+        Text("Navigation View")
+    }
+}
