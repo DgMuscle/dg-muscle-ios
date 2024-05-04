06c084384cacb8680a23d465b7d3dc8ed8f789af
신동규 <donggyu9410@gmail.com>
2024-05-04 14:41:47 +0900

    feat: QuickAction

--- dg-muscle-ios/presenter/navigation/coordinator/CoordinatorV2.swift
+++ dg-muscle-ios/presenter/navigation/coordinator/CoordinatorV2.swift
@@ -9,18 +9,21 @@ import Foundation
 import SwiftUI
 import Combine
 
-var coordinatorv2InstanceCount: Int = 0
-
 final class CoordinatorV2: ObservableObject {
+    static var instanceCount: Int = 0
     @Binding var path: NavigationPath
     
     lazy var exercise = ExerciseCoordinatorV2(path: $path)
     lazy var history = HistoryCoordinatorV2(path: $path)
     lazy var main = MainCoordinatorV2(path: $path)
     
+    private let remoteCoordinator: RemoteCoordinator = RemoteCoordinator.shared
+    private let getTodayHistoryUsecase: GetTodayHistoryUsecase =
+        .init(historyRepository: HistoryRepositoryData.shared,
+              today: Date())
     private var cancellables = Set<AnyCancellable>()
     init(path: Binding<NavigationPath>) {
-        coordinatorv2InstanceCount += 1
+        Self.instanceCount += 1
         self._path = path
         bind()
     }
@@ -32,6 +35,26 @@ final class CoordinatorV2: ObservableObject {
     }
     
     private func bind() {
-        
+        if Self.instanceCount == 2 {
+            remoteCoordinator.$quickAction
+                .receive(on: DispatchQueue.main)
+                .compactMap({ $0 })
+                .sink { [weak self] quickaction in
+                    guard let self else { return }
+                    let history: HistoryV
+                    
+                    if let domain = getTodayHistoryUsecase.implement() {
+                        history = .init(from: domain)
+                    } else {
+                        history = .init()
+                    }
+                    
+                    switch quickaction.type {
+                    case .record:
+                        self.history.historyForm(history)
+                    }
+                }
+                .store(in: &cancellables)
+        }
     }
 }
