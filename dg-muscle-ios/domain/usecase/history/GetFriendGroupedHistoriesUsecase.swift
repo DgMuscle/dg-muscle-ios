8f35c2e9cdd86f1db4c59550537626c6f4c370f7
신동규 <donggyu9410@gmail.com>
2024-05-05 14:18:43 +0900

    GetFriendGroupedHistoriesUsecase

---
+++ dg-muscle-ios/domain/usecase/history/GetFriendGroupedHistoriesUsecase.swift
@@ -0,0 +1,20 @@
+//
+//  GetFriendGroupedHistoriesUsecase.swift
+//  dg-muscle-ios
+//
+//  Created by 신동규 on 5/5/24.
+//
+
+import Foundation
+
+final class GetFriendGroupedHistoriesUsecase {
+    let historyRepository: HistoryRepository
+    
+    init(historyRepository: HistoryRepository) {
+        self.historyRepository = historyRepository
+    }
+    
+    func implement() {
+        
+    }
+}
