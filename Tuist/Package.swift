6658716e562a784644be38c9c97d60803fbc13ab
신동규 <donggyu9410@gmail.com>
2024-05-13 23:47:54 +0900

    preparing

---
+++ Tuist/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+import PackageDescription
+let projectName = "dg-muscle-ios"
+
+#if TUIST
+    import ProjectDescription
+
+    let packageSettings = PackageSettings(
+        // Customize the product types for specific package product
+        // Default is .staticFramework
+        // productTypes: ["Alamofire": .framework,] 
+        productTypes: [:]
+    )
+#endif
+
+let package = Package(
+    name: projectName,
+    dependencies: [
+        // Add your own dependencies here:
+        // .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
+        // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
+    ]
+)
