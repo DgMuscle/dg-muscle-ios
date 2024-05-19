298744cbfa0609db960368928667aa92e4404d85
신동규 <donggyu9410@gmail.com>
2024-05-19 16:06:32 +0900

    init ExerciseManagerView

---
+++ dg-muscle-ios/sources/Presentation/Exercise/View/ExerciseManagerView.swift
@@ -0,0 +1,24 @@
+//
+//  ExerciseManagerView.swift
+//  Exercise
+//
+//  Created by 신동규 on 5/19/24.
+//
+
+import SwiftUI
+
+public struct ExerciseManagerView: View {
+    
+    public init() {
+        
+    }
+    
+    public var body: some View {
+        Text("ExerciseManagerView")
+    }
+}
+
+#Preview {
+    return ExerciseManagerView()
+        .preferredColorScheme(.dark)
+}
