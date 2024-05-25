9160cc90ccd1d223475e6c57d72793f1299a8bca
신동규 <donggyu9410@gmail.com>
2024-05-25 17:52:54 +0900

    add backgroundImageURL

--- dg-muscle-ios/sources/Domain/User/Model/User.swift
+++ dg-muscle-ios/sources/Domain/User/Model/User.swift
@@ -11,18 +11,21 @@ public struct User {
     public let uid: String
     public var displayName: String?
     public var photoURL: URL?
+    public var backgroundImageURL: URL?
     public var heatMapColor: HeatMapColor
     public var fcmToken: String?
     
     public init(
         uid: String, 
         displayName: String? = nil,
+        backgroundImageURL: URL?,
         photoURL: URL? = nil,
         heatMapColor: HeatMapColor,
         fcmToken: String?
     ) {
         self.uid = uid
         self.displayName = displayName
+        self.backgroundImageURL = backgroundImageURL
         self.photoURL = photoURL
         self.heatMapColor = heatMapColor
         self.fcmToken = fcmToken
