6941329f0c48cbe0e9f3fc2af5353b5480b535d9
신동규 <donggyu9410@gmail.com>
2024-05-26 18:36:40 +0900

    SearchUsersExceptForMyFriendsUsecase

---
+++ dg-muscle-ios/sources/Presentation/Friend/UserList/UserListViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  UserListViewModel.swift
+//  Friend
+//
+//  Created by 신동규 on 5/26/24.
+//
+
+import Foundation
+import Domain
+import Combine
+
+final class UserListViewModel: ObservableObject {
+    private var cancellables = Set<AnyCancellable>()
+}
