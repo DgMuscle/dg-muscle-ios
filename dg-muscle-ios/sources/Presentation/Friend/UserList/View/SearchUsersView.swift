4067553eeb5b60ca6af245408df9a3de2fbfbf5b
신동규 <donggyu9410@gmail.com>
2024-06-02 00:27:33 +0900

    requestFriend

--- dg-muscle-ios/sources/Presentation/Friend/UserList/View/SearchUsersView.swift
+++ dg-muscle-ios/sources/Presentation/Friend/UserList/View/SearchUsersView.swift
@@ -8,10 +8,12 @@
 import SwiftUI
 import Domain
 import MockData
+import Common
 
 struct SearchUsersView: View {
     
     @StateObject var viewModel: SearchUsersViewModel
+    @State var selectedUser: Common.User?
     
     init(
         friendRepository: FriendRepository,
@@ -23,15 +25,44 @@ struct SearchUsersView: View {
     
     var body: some View {
         List {
+            
+            if let status = viewModel.status {
+                switch status {
+                case .loading:
+                    Common.StatusView(status: status)
+                case .success, .error:
+                    Common.StatusView(status: status)
+                        .onAppear {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                                viewModel.status = nil
+                            }
+                        }
+                }
+                
+                
+            }
+            
             TextField("Search users by display name", text: $viewModel.query)
             
             Section("Searched") {
                 ForEach(viewModel.users, id: \.self) { user in
-                    FriendListItemView(friend: .init(domain: user.domain))
+                    Button {
+                        selectedUser = user
+                    } label: {
+                        FriendListItemView(friend: .init(domain: user.domain))
+                    }
                 }
             }
         }
+        .animation(.default, value: viewModel.status)
         .scrollIndicators(.hidden)
+        .sheet(item: $selectedUser) { user in
+            RequestFriendView(user: user) { userId in
+                viewModel.requestFriend(userId: userId)
+                selectedUser = nil
+            }
+            .presentationDetents([.height(300)])
+        }
     }
 }
 
