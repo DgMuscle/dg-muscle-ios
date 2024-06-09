90f8c13b10ad49a3a070c08d7b3f75fbfdc0608b
신동규 <donggyu9410@gmail.com>
2024-06-09 18:23:33 +0900

    feat: Quick open by widget

--- dg-muscle-ios/sources/App/Delegate/SceneDelegate.swift
+++ dg-muscle-ios/sources/App/Delegate/SceneDelegate.swift
@@ -16,6 +16,12 @@ class SceneDelegate: NSObject, UIWindowSceneDelegate {
         if let shortcutItem = connectionOptions.shortcutItem {
             print("dg: shortcutItem is \(shortcutItem)")
         }
+        
+        if let urlContext = connectionOptions.urlContexts.first {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                self.handleURL(url: urlContext.url)
+            }
+        }
     }
     
     func windowScene(_ windowScene: UIWindowScene, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
@@ -24,39 +30,41 @@ class SceneDelegate: NSObject, UIWindowSceneDelegate {
     }
     
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
-        for context in URLContexts {
-            let url = context.url
-            guard let scheme = url.scheme, let host = url.host() else { return }
-            guard scheme == "dgmuscle" else { return }
+        guard let url = URLContexts.map({ $0.url }).first else { return }
+        handleURL(url: url)
+    }
+    
+    private func handleURL(url: URL) {
+        guard let scheme = url.scheme, let host = url.host() else { return }
+        guard scheme == "dgmuscle" else { return }
+        
+        switch host {
+        case "friend":
+            var anchor: PageAnchorView.Page = .friend
             
-            switch host {
+            let anchorString = URLManager.shared.getParameter(url: url, name: "anchor")
+            
+            switch anchorString {
             case "friend":
-                var anchor: PageAnchorView.Page = .friend
-                
-                let anchorString = URLManager.shared.getParameter(url: url, name: "anchor")
-                
-                switch anchorString {
-                case "friend":
-                    anchor = .friend
-                case "request":
-                    anchor = .request
-                case "search":
-                    anchor = .search
-                default: break
-                }
-                
-                coordinator?.friendMainView(anchor: anchor)
-            case "profile":
-                coordinator?.profile()
-            case "exercisemanage":
-                coordinator?.exerciseManage()
-            case "heatmapcolorselect":
-                coordinator?.heatMapColorSelectView()
-            case "history":
-                let historyId = URLManager.shared.getParameter(url: url, name: "id")
-                coordinator?.historyFormStep1(historyId: historyId)
+                anchor = .friend
+            case "request":
+                anchor = .request
+            case "search":
+                anchor = .search
             default: break
             }
+            
+            coordinator?.friendMainView(anchor: anchor)
+        case "profile":
+            coordinator?.profile()
+        case "exercisemanage":
+            coordinator?.exerciseManage()
+        case "heatmapcolorselect":
+            coordinator?.heatMapColorSelectView()
+        case "history":
+            let historyId = URLManager.shared.getParameter(url: url, name: "id")
+            coordinator?.historyFormStep1(historyId: historyId)
+        default: break
         }
     }
 }
