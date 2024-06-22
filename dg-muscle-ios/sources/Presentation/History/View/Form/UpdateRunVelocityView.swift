dad6b085bb4ad1e6a45e84edf44f638d98e70f4d
신동규 <donggyu9410@gmail.com>
2024-06-22 13:25:49 +0900

    feat: UpdateRunVelocityView ui

--- dg-muscle-ios/sources/Presentation/History/View/Form/UpdateRunVelocityView.swift
+++ dg-muscle-ios/sources/Presentation/History/View/Form/UpdateRunVelocityView.swift
@@ -8,27 +8,64 @@
 import SwiftUI
 import Domain
 import MockData
+import Common
 
 public struct UpdateRunVelocityView: View {
     
     @State private var velocity: Double
+    @FocusState private var focus
+    let color: Common.HeatMapColor
     
     private let updateRunVelocityUsecase: UpdateRunVelocityUsecase
+    private let getHeatMapColorUsecase: GetHeatMapColorUsecase
     
+    private var numberFormatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+
     public init(
         runRepository: RunRepository,
+        userRepository: UserRepository,
         velocity: Double
     ) {
         updateRunVelocityUsecase = .init(runRepository: runRepository)
+        getHeatMapColorUsecase = .init(userRepository: userRepository)
         _velocity = .init(initialValue: velocity)
+        color = .init(domain: getHeatMapColorUsecase.implement())
     }
     
     public var body: some View {
-        Text("UpdateRunVelocityView")
+        VStack(spacing: 20) {
+            HStack {
+                Spacer()
+                TextField("Velocity", value: $velocity, formatter: numberFormatter)
+                    .fontWeight(.black)
+                    .font(.system(size: 40))
+                Text("km/h")
+                Spacer()
+            }
+            
+            Common.GradientButton(action: {
+                print("save")
+            }, text: "SAVE", backgroundColor: color.color)
+        }
+        .padding()
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                focus.toggle()
+            }
+        }
     }
 }
 
 #Preview {
-    UpdateRunVelocityView(runRepository: RunRepositoryMock(), velocity: 6.7)
-        .preferredColorScheme(.dark)
+    UpdateRunVelocityView(
+        runRepository: RunRepositoryMock(),
+        userRepository: UserRepositoryMock(),
+        velocity: 6.72
+    )
+    .preferredColorScheme(.dark)
 }
