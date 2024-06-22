1852add0a83eb58b601d4bfc48e4050f305bb287
신동규 <donggyu9410@gmail.com>
2024-06-22 12:53:45 +0900

    feat: mock data layer

--- dg-muscle-ios/sources/MockData/Repository/RunRepositoryMock.swift
+++ dg-muscle-ios/sources/MockData/Repository/RunRepositoryMock.swift
@@ -1,13 +1,13 @@
 //
-//  RunRepositoryImpl.swift
-//  DataLayer
+//  RunRepositoryMock.swift
+//  MockData
 //
 //  Created by 신동규 on 6/22/24.
 //
 
-import Combine
 import Domain
+import Combine
 
-public final class RunRepositoryImpl: RunRepository {
+public final class RunRepositoryMock: RunRepository {
     public var velocitySubject: PassthroughSubject<Double, Never> = .init()
 }
