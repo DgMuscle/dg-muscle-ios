d24b7e8edbe4e8a63b0774047d02856a0c97db0a
신동규 <donggyu9410@gmail.com>
2024-06-22 17:36:21 +0900

    feat: ui

--- dg-muscle-ios/sources/Presentation/My/My/View/DeleteAccountConfirmView.swift
+++ dg-muscle-ios/sources/Presentation/My/My/View/DeleteAccountConfirmView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import Domain
 import MockData
+import Common
 
 public struct DeleteAccountConfirmView: View {
     
@@ -18,7 +19,28 @@ public struct DeleteAccountConfirmView: View {
     }
     
     public var body: some View {
-        Text("DeleteAccountConfirmView")
+        ZStack {
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Are you really want to delete your account?")
+                    Spacer()
+                }
+                
+                Text("This cannot be reversed.")
+                    .italic()
+                    .foregroundStyle(.red)
+                
+            }
+            
+            VStack {
+                Spacer()
+                Common.GradientButton(action: {
+                    URLManager.shared.open(url: "dgmuscle://pop")
+                    deleteAccountTriggerUsecase.implement()
+                }, text: "DELETE", backgroundColor: .red)
+            }
+        }
+        .padding()
     }
 }
 
