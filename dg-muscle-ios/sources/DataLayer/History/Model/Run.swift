606a57fcfbdce53dd701b6021b3cac5ec0fecabb
donggyu <donggyu_00@naver.com>
2024-06-25 15:16:17 +0900

    feat: Run Model

---
+++ dg-muscle-ios/sources/DataLayer/History/Model/Run.swift
@@ -0,0 +1,27 @@
+//
+//  Run.swift
+//  DataLayer
+//
+//  Created by Donggyu Shin on 6/25/24.
+//
+
+import Foundation
+import Domain
+
+struct Run {
+    let duration: Int
+    let distance: Double
+    
+    init(domain: Domain.Run) {
+        self.duration = domain.duration
+        self.distance = domain.distance
+    }
+    
+    var domain: Domain.Run {
+        .init(
+            duration: duration,
+            distance: distance
+        )
+    }
+}
+
