4a6518f57da1680ee880e8083f718059af176a8c
신동규 <donggyu9410@gmail.com>
2024-07-04 22:18:34 +0900

    feat: DateToSelectHistoryView

--- dg-muscle-ios/sources/Presentation/History/View/List/DateToSelectHistoryView.swift
+++ dg-muscle-ios/sources/Presentation/History/View/List/DateToSelectHistoryView.swift
@@ -6,14 +6,48 @@
 //
 
 import SwiftUI
+import Domain
+import MockData
+import Common
 
 public struct DateToSelectHistoryView: View {
     
-    public init() {
-        
+    let pushDateToSelectHistoryUsecase: PushDateToSelectHistoryUsecase
+    @State var date: Date
+    
+    private let now = Date()
+    
+    public init(historyRepository: HistoryRepository) {
+        pushDateToSelectHistoryUsecase = .init(historyRepository: historyRepository)
+        self.date = Calendar.current.date(byAdding: .day, value: -1, to: Date()) ?? Date()
     }
     
     public var body: some View {
-        Text("DateToSelectHistoryView")
+        VStack {
+            DatePicker("Date to select record", selection: $date, displayedComponents: .date)
+            
+            HStack {
+                Spacer()
+                Button("Select") {
+                    URLManager.shared.open(url: "dgmuscle://pop")
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        pushDateToSelectHistoryUsecase.implement(date: date)
+                    }
+                }
+                .buttonStyle(.bordered)
+            }
+        }
+        .padding()
+        .onChange(of: date) { oldValue, newValue in
+            if newValue >= now || Calendar.current.isDate(newValue, inSameDayAs: now) {
+                date = oldValue
+            }
+        }
     }
+}
+
+#Preview {
+    DateToSelectHistoryView(historyRepository: HistoryRepositoryMock())
+        .preferredColorScheme(.dark)
 }
