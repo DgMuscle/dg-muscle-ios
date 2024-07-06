30ec538acc0ded30d4c552297f1e4574aeffa42a
신동규 <donggyu9410@gmail.com>
2024-07-06 16:15:28 +0900

    feat: FriendAssembly

---
+++ dg-muscle-ios/sources/App/DI/FriendAssembly.swift
@@ -0,0 +1,39 @@
+//
+//  FriendAssembly.swift
+//  App
+//
+//  Created by 신동규 on 7/6/24.
+//
+
+import Swinject
+import Friend
+import Domain
+import Presentation
+import Foundation
+
+public struct FriendAssembly: Assembly {
+    public func assemble(container: Swinject.Container) {
+        container.register(FriendMainView.self) { (resolver, page: PageAnchorView.Page) in
+            
+            let friendRepository = resolver.resolve(FriendRepository.self)!
+            let userRepository = resolver.resolve(UserRepository.self)!
+            
+            return FriendMainView(
+                friendRepository: friendRepository,
+                userRepository: userRepository,
+                page: page
+            )
+        }
+        
+        container.register(FriendHistoryView.self) { (resolver, friendId: String, today: Date) in
+            
+            let friendRepository = resolver.resolve(FriendRepository.self)!
+            
+            return FriendHistoryView(
+                friendRepository: friendRepository,
+                friendId: friendId,
+                today: today
+            )
+        }
+    }
+}
