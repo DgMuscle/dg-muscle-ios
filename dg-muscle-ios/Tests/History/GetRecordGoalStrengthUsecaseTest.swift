f9b6020b620b927daac43c6d6582490e12d8523d
신동규 <donggyu9410@gmail.com>
2024-07-13 20:46:04 +0900

    feat: add test case

---
+++ dg-muscle-ios/Tests/History/GetRecordGoalStrengthUsecaseTest.swift
@@ -0,0 +1,59 @@
+//
+//  GetRecordGoalStrengthUsecaseTest.swift
+//  AppTests
+//
+//  Created by 신동규 on 7/13/24.
+//
+
+import XCTest
+import Domain
+
+final class GetRecordGoalStrengthUsecaseTest: XCTestCase {
+    
+    func testMoreWeight() {
+        // given
+        let useCase = GetRecordGoalStrengthUsecase()
+        
+        let record: ExerciseRecord = .init(
+            id: UUID().uuidString,
+            exerciseId: "squat",
+            sets: [
+                .init(id: UUID().uuidString, unit: .kg, reps: 15, weight: 60),
+                .init(id: UUID().uuidString, unit: .kg, reps: 15, weight: 60),
+                .init(id: UUID().uuidString, unit: .kg, reps: 5, weight: 60),
+                .init(id: UUID().uuidString, unit: .kg, reps: 5, weight: 60),
+                .init(id: UUID().uuidString, unit: .kg, reps: 5, weight: 60)
+            ]
+        )
+        
+        // when
+        let goal = useCase.implement(previousRecord: record)
+        
+        // then
+        XCTAssertEqual(goal?.weight, 65)
+        XCTAssertEqual(goal?.reps, 5)
+    }
+
+    func testSameWeigth() {
+        // given
+        let useCase = GetRecordGoalStrengthUsecase()
+        
+        let record: ExerciseRecord = .init(
+            id: UUID().uuidString,
+            exerciseId: "squat",
+            sets: [
+                .init(id: UUID().uuidString, unit: .kg, reps: 15, weight: 60),
+                .init(id: UUID().uuidString, unit: .kg, reps: 15, weight: 60),
+                .init(id: UUID().uuidString, unit: .kg, reps: 15, weight: 60)
+            ]
+        )
+        
+        // when
+        let goal = useCase.implement(previousRecord: record)
+        
+        // then
+        XCTAssertEqual(goal?.weight, 60)
+        XCTAssertEqual(goal?.reps, 5)
+    }
+
+}
---
+++ dg-muscle-ios/sources/Domain/History/Usecase/CheckStrengthGoalAchievedUsecase.swift
@@ -0,0 +1,20 @@
+//
+//  CheckStrengthGoalAchievedUsecase.swift
+//  Domain
+//
+//  Created by 신동규 on 7/13/24.
+//
+
+import Foundation
+
+public final class CheckStrengthGoalAchievedUsecase {
+    public init() { }
+    
+    public func implement(goal: ExerciseSet, record: ExerciseRecord) -> Bool {
+        var result: Bool = false
+        
+        
+        
+        return result
+    }
+}
