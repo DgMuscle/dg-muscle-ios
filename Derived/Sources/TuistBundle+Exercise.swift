7c64d5eaee5bddb967aff53c0f298a5fa0f93fb3
신동규 <donggyu9410@gmail.com>
2024-07-17 22:59:29 +0900

    chore

--- Derived/Sources/TuistBundle+Exercise.swift
+++ Derived/Sources/TuistBundle+Exercise.swift
@@ -4,7 +4,7 @@
 import Foundation// MARK: - Swift Bundle Accessor for Frameworks
 private class BundleFinder {}
 extension Foundation.Bundle {
-/// Since Exercise is a framework, the bundle for classes within this module can be used directly.
+/// Since Exercise is a dynamic framework, the bundle for classes within this module can be used directly.
 static let module = Bundle(for: BundleFinder.self)
 }// MARK: - Objective-C Bundle Accessor
 @objc
