4437a8b5fb18103f3d1dd9c06347e37ea7569701
신동규 <donggyu9410@gmail.com>
2024-07-21 18:18:40 +0900

    feat: RapidRepositoryImpl

---
+++ dg-muscle-ios/sources/DataLayer/Rapid/Repository/RapidRepositoryImpl.swift
@@ -0,0 +1,26 @@
+//
+//  RapidRepositoryImpl.swift
+//  DataLayer
+//
+//  Created by 신동규 on 7/21/24.
+//
+
+import Foundation
+import Combine
+import Domain
+
+public final class RapidRepositoryImpl: RapidRepository {
+    public static let shared = RapidRepositoryImpl()
+    
+    public var exercises: AnyPublisher<[Domain.RapidExerciseDomain], Never> {
+        $_exercises.eraseToAnyPublisher()
+    }
+    
+    @Published private var _exercises: [Domain.RapidExerciseDomain] = []
+    
+    public func get() -> [Domain.RapidExerciseDomain] {
+        _exercises
+    }
+    
+    private init() { }
+}
