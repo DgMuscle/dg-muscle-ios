ea2cb7c7cc2d7528a0987c95d8ee49a37a860bf4
신동규 <donggyu9410@gmail.com>
2024-07-21 21:27:18 +0900

    feat: Add Model

--- dg-muscle-ios/sources/Presentation/Rapid/Model/RapidExerciseThumbnailPresentation.swift
+++ dg-muscle-ios/sources/Presentation/Rapid/Model/RapidExerciseThumbnailPresentation.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import Domain
+
+struct RapidExerciseThumbnailPresentation {
+    public let id: String
+    public let name: String
+    public let bodyPart: RapidBodyPartPresentation
+    
+    init(domain: Domain.RapidExerciseDomain) {
+        id = domain.id
+        name = domain.name
+        bodyPart = .init(domain: domain.bodyPart) 
+    }
+}
