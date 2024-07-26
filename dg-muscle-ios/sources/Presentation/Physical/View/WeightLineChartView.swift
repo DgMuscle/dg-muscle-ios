b89c56859d9ee41bfa19ecd48e4183b1ee4b2792
donggyu <donggyu_00@naver.com>
2024-07-26 17:40:40 +0900

    feat: WeightLineChartView

---
+++ dg-muscle-ios/sources/Presentation/Physical/View/WeightLineChartView.swift
@@ -0,0 +1,17 @@
+//
+//  WeightLineChartView.swift
+//  App
+//
+//  Created by Donggyu Shin on 7/26/24.
+//
+
+import SwiftUI
+
+struct WeightLineChartView: View {
+    
+    let data: [WeightPresentation]
+    
+    var body: some View {
+        Text("WeightLineChartView")
+    }
+}
