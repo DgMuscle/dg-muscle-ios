c63e590de1245cbc5bfdee8a6d8ab63fad76aca9
신동규 <donggyu9410@gmail.com>
2024-07-31 23:19:12 +0900

    delete friend profile view

--- dg-muscle-ios/sources/Presentation/Friend/List/View/FriendProfileView.swift
+++ dg-muscle-ios/sources/Presentation/Friend/List/View/FriendProfileView.swift
@@ -16,82 +16,16 @@ struct FriendProfileView: View {
     let friend: Friend
     @Binding var selectedFriend: Friend?
     
-    private let profileImageSize: CGFloat = 80
     
     var body: some View {
-        ZStack {
-            if let backgroundURL = friend.backgroundImageURL {
-                Rectangle()
-                    .fill(.clear)
-                    .background(
-                        KFImage(backgroundURL)
-                            .resizable()
-                            .scaledToFill()
-                    )
-                    .ignoresSafeArea()
-            }
-            
-            VStack(spacing: 16) {
-                
-                HStack {
-                    Spacer()
-                    Button("close", systemImage: "xmark") {
-                        selectedFriend = nil
-                    }
-                    .foregroundStyle(Color(uiColor: .label))
-                }
-                .padding(.horizontal)
-                
-                Spacer()
-                
-                if let url = friend.photoURL {
-                    KFImage(url)
-                        .resizable()
-                        .frame(width: profileImageSize, height: profileImageSize)
-                        .scaledToFit()
-                        .clipShape(RoundedRectangle(cornerRadius: 16))
-                } else {
-                    RoundedRectangle(cornerRadius: 16)
-                        .fill(Color(uiColor: .tertiarySystemBackground))
-                        .frame(width: profileImageSize, height: profileImageSize)
-                        .overlay {
-                            Image(systemName: "person")
-                                .font(.largeTitle)
-                        }
-                }
-                
-                Text(friend.displayName ?? friend.id)
-                    .fontWeight(.black)
-                
-                if let link = friend.link {
-                    VStack {
-                        HStack {
-                            Text(link.absoluteString)
-                            Image(systemName: "link")
-                        }
-                        .onTapGesture {
-                            URLManager.shared.open(url: link)
-                        }
-                        Divider()
-                            .frame(height: 1)
-                    }
-                }
-                
-                GradientButton(action: {
-                    selectedFriend = nil
-                    URLManager.shared.open(url: "dgmuscle://friendhistory?id=\(friend.uid)")
-                }, text: "Workout History", backgroundColor: friend.heatMapColor.color)
-                .padding(.horizontal)
-            }
-        }
+        Text("Friend Profile View")
     }
 }
 
 #Preview {
-    let view = FriendProfileView(
+    FriendProfileView(
         friend: .init(domain: USER_DG),
         selectedFriend: .constant(.init(domain: USER_DG))
     )
-        .preferredColorScheme(.dark)
-    return view
+    .preferredColorScheme(.dark)
 }
