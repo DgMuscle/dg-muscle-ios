c2a70d403a59fd15a791800e5e0f96290f388e3b
donggyu <donggyu_00@naver.com>
2024-08-06 12:30:22 +0900

    repository

---
+++ dg-muscle-ios/sources/Domain/Weight/Repository/WeightRepository.swift
@@ -0,0 +1,15 @@
+//
+//  WeightRepository.swift
+//  Domain
+//
+//  Created by Donggyu Shin on 8/6/24.
+//
+
+import Foundation
+import Combine
+
+public protocol WeightRepository {
+    var weights: AnyPublisher<[WeightDomain], Never> { get }
+    
+    func get() -> [WeightDomain]
+}
