96035e53b4c0057c4d72f4392303254851d573a9
신동규 <donggyu9410@gmail.com>
2024-08-06 22:26:22 +0900

    feat: preview

--- dg-muscle-ios/sources/Presentation/Weight/View/WeightLineChart/WeightLineChartView.swift
+++ dg-muscle-ios/sources/Presentation/Weight/View/WeightLineChart/WeightLineChartView.swift
@@ -7,27 +7,37 @@
 
 import SwiftUI
 import Charts
+import MockData
+import Domain
 
 struct WeightLineChartView: View {
     
-    let data: [WeightPresentation]
+    @StateObject var viewModel: WeightLineChartViewModel
+    
+    init(weightRepository: WeightRepository) {
+        _viewModel = .init(wrappedValue: .init(weightRepository: weightRepository))
+    }
     
     private let dotSize: CGFloat = 6
     private let cellSize: CGFloat = 30
     
     var body: some View {
-        Chart(data, id: \.self) { data in
+        Chart(viewModel.weights, id: \.self) { data in
             LineMark(x: .value("date", data.date, unit: .day),
                      y: .value("weight", data.value))
             .interpolationMethod(.catmullRom)
             .symbol {
-                Circle()
-                    .frame(width: dotSize, height: dotSize)
+                Circle().frame(width: dotSize, height: dotSize)
             }
         }
         .chartScrollableAxes(.horizontal)
         .chartScrollPosition(initialX: Int.max)
         .frame(height: 300)
-        .chartYScale(domain: 50...80) // 범위 구하는 UseCase 추가 필요
+        .chartYScale(domain: viewModel.range.0...viewModel.range.1)
     }
+}
+
+#Preview {
+    return WeightLineChartView(weightRepository: WeightRepositoryMock())
+        .preferredColorScheme(.dark)
 }
