8a9730cd65a3571f045a2c81e83cede1b4b3a09a
donggyu <donggyu_00@naver.com>
2024-08-06 13:53:36 +0900

    request permission

--- dg-muscle-ios/sources/DataLayer/Weight/Repository/WeightRepositoryImpl.swift
+++ dg-muscle-ios/sources/DataLayer/Weight/Repository/WeightRepositoryImpl.swift
@@ -8,16 +8,42 @@
 import Foundation
 import Combine
 import Domain
+import HealthKit
 
 public final class WeightRepositoryImpl: WeightRepository {
+    public static let shared = WeightRepositoryImpl()
+    
+    private let healthStore = HKHealthStore()
+    private let allTypes = Set([HKObjectType.quantityType(forIdentifier: .bodyMass)!])
+
     public var weights: AnyPublisher<[Domain.WeightDomain], Never> { $_weights.eraseToAnyPublisher() }
     @Published var _weights: [Domain.WeightDomain] = []
     
-    public init() {
-        
+    private init() {
+        Task {
+            try await requestPermission()
+            fetchWeights()
+        }
     }
     
     public func get() -> [Domain.WeightDomain] {
         _weights
+    }
+    
+    private func fetchWeights() {
+        
+    }
+    
+    private func requestPermission() async throws {
+        return try await withCheckedThrowingContinuation { continuation in
+            healthStore.requestAuthorization(toShare: allTypes, read: allTypes) { (success, error) in
+                
+                if let error {
+                    continuation.resume(throwing: error)
+                } else {
+                    continuation.resume()
+                }
+            }
+        }
     }
 }
