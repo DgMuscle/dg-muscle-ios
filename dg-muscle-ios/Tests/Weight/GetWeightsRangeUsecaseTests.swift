2790a58597f3b1710e6724f578f3a224f96bcfd7
donggyu <donggyu_00@naver.com>
2024-08-06 16:57:45 +0900

    feat: GetWeightsRangeUsecase

---
+++ dg-muscle-ios/Tests/Weight/GetWeightsRangeUsecaseTests.swift
@@ -0,0 +1,21 @@
+//
+//  GetWeightsRangeUsecaseTests.swift
+//  AppTests
+//
+//  Created by Donggyu Shin on 8/6/24.
+//
+
+import XCTest
+import Domain
+import MockData
+
+final class GetWeightsRangeUsecaseTests: XCTestCase {
+
+    func testExample() throws {
+        let usecase = GetWeightsRangeUsecase()
+        let repository: WeightRepository = WeightRepositoryMock()
+        let result = usecase.implement(weights: repository.get())
+        XCTAssertEqual(result.0, 58)
+        XCTAssertEqual(result.1, 72)
+    }
+}
