d910264c94caffbe2d0b5301ad56537bd5fc24e0
신동규 <donggyu9410@gmail.com>
2024-08-06 22:21:41 +0900

    feat: WeightLineChartViewModel

---
+++ dg-muscle-ios/sources/Presentation/Weight/View/WeightLineChart/WeightLineChartViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  WeightLineChartViewModel.swift
+//  Weight
+//
+//  Created by 신동규 on 8/6/24.
+//
+
+import Foundation
+import Combine
+import Domain
+
+final class WeightLineChartViewModel: ObservableObject {
+    @Published var weights: [WeightPresentation] = []
+    @Published var range: (Double, Double) = (0, 0)
+    
+    private let subscribeWeightsUsecase: SubscribeWeightsUsecase
+    private let getWeightsRangeUsecase: GetWeightsRangeUsecase
+    
+    init(weightRepository: WeightRepository) {
+        subscribeWeightsUsecase = .init(weightRepository: weightRepository)
+        getWeightsRangeUsecase = .init()
+        
+        bind()
+    }
+    
+    private func bind() {
+        subscribeWeightsUsecase
+            .implement()
+            .compactMap({ [weak self] weightDomains -> [WeightPresentation]? in
+                guard let self else { return nil }
+                return weightDomains.map({ .init(domain: $0) })
+            })
+            .receive(on: DispatchQueue.main)
+            .assign(to: &$weights)
+        
+        subscribeWeightsUsecase
+            .implement()
+            .compactMap({ [weak self] weights -> (Double, Double)? in
+                guard let self else { return nil }
+                return getWeightsRangeUsecase.implement(weights: weights)
+            })
+            .assign(to: &$range)
+    }
+}
