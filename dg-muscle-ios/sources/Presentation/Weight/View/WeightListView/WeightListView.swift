5a47f79fe3c5f9eb60e728cd5eb8e82acd3d3746
신동규 <donggyu9410@gmail.com>
2024-08-07 23:14:34 +0900

    feat: listview

--- dg-muscle-ios/sources/Presentation/Weight/View/WeightListView/WeightListView.swift
+++ dg-muscle-ios/sources/Presentation/Weight/View/WeightListView/WeightListView.swift
@@ -12,13 +12,28 @@ import MockData
 public struct WeightListView: View {
     
     @StateObject var viewModel: WeightListViewModel
+    private let weightRepository: WeightRepository
     
     public init(weightRepository: WeightRepository) {
+        self.weightRepository = weightRepository
         _viewModel = .init(wrappedValue: .init(weightRepository: weightRepository))
     }
     
     public var body: some View {
-        Text("WeightListView")
+        ScrollView {
+            WeightLineChartView(
+                weightRepository: weightRepository,
+                weights: viewModel.weights,
+                range: viewModel.weightRange
+            )
+            
+            VStack {
+                ForEach(viewModel.sections, id: \.self) { section in
+                    WeightSectionView(section: section)
+                }
+            }
+        }
+        .scrollIndicators(.hidden)
     }
 }
 
