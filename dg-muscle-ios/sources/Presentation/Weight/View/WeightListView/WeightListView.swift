63a19db3b8a5c10b132d1f2024a6480715420f80
donggyu <donggyu_00@naver.com>
2024-08-07 16:52:53 +0900

    feat: weightlistview

---
+++ dg-muscle-ios/sources/Presentation/Weight/View/WeightListView/WeightListView.swift
@@ -0,0 +1,28 @@
+//
+//  WeightListView.swift
+//  Weight
+//
+//  Created by Donggyu Shin on 8/7/24.
+//
+
+import SwiftUI
+import Domain
+import MockData
+
+public struct WeightListView: View {
+    
+    @StateObject var viewModel: WeightListViewModel
+    
+    public init(weightRepository: WeightRepository) {
+        _viewModel = .init(wrappedValue: .init(weightRepository: weightRepository))
+    }
+    
+    public var body: some View {
+        Text("WeightListView")
+    }
+}
+
+#Preview {
+    return WeightListView(weightRepository: WeightRepositoryMock())
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+}
