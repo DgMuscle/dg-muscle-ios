971a5e38bd43c10110f6ba6b8548fadebaf23a35
donggyu <donggyu_00@naver.com>
2024-08-07 17:40:00 +0900

    feat: add date

--- dg-muscle-ios/sources/Presentation/Weight/Model/WeightSection.swift
+++ dg-muscle-ios/sources/Presentation/Weight/Model/WeightSection.swift
@@ -10,4 +10,10 @@ import Foundation
 struct WeightSection: Hashable {
     let yyyyMM: String
     let weights: [WeightPresentation]
+    
+    var date: Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMM"
+        return dateFormatter.date(from: yyyyMM) ?? Date()
+    }
 }
