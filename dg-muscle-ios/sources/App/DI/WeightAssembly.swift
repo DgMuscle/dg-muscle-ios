f32a8826c35ad594a5d3ca7a9c4520fd0aef439c
donggyu <donggyu_00@naver.com>
2024-08-07 16:56:10 +0900

    register DI

---
+++ dg-muscle-ios/sources/App/DI/WeightAssembly.swift
@@ -0,0 +1,19 @@
+//
+//  WeightAssembly.swift
+//  App
+//
+//  Created by Donggyu Shin on 8/7/24.
+//
+
+import Swinject
+import Weight
+import Domain
+
+public struct WeightAssembly: Assembly {
+    public func assemble(container: Swinject.Container) {
+        container.register(WeightListView.self) { resolver in
+            let weightRepository = resolver.resolve(WeightRepository.self)!
+            return WeightListView(weightRepository: weightRepository)
+        }
+    }
+}
