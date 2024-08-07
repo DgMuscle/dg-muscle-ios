7deced77a6e52ea6765aa27044609cad0ecfbc0e
donggyu <donggyu_00@naver.com>
2024-08-07 17:26:26 +0900

    feat: weightsection

---
+++ dg-muscle-ios/sources/Presentation/Weight/Model/WeightSection.swift
@@ -0,0 +1,13 @@
+//
+//  WeightSection.swift
+//  Weight
+//
+//  Created by Donggyu Shin on 8/7/24.
+//
+
+import Foundation
+
+struct WeightSection: Hashable {
+    let yyyyMM: String
+    let weights: [WeightPresentation]
+}
