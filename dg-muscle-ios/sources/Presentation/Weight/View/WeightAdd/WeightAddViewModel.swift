3ac8a36323099ecbc49865e1921a1de18d6a27c0
donggyu <donggyu_00@naver.com>
2024-08-08 15:55:24 +0900

    feat: GetRecentWeightUsecase

--- dg-muscle-ios/sources/Presentation/Weight/View/WeightAdd/WeightAddViewModel.swift
+++ dg-muscle-ios/sources/Presentation/Weight/View/WeightAdd/WeightAddViewModel.swift
@@ -10,9 +10,17 @@ import Combine
 import Domain
 
 final class WeightAddViewModel: ObservableObject {
+    
+    @Published var selectedDate: Date = .init()
+    @Published var value: Double = 0
+    
     let postWeightUsecase: PostWeightUsecase
+    let getRecentWeightUsecase: GetRecentWeightUsecase
     
     init(weightRepository: WeightRepository) {
         postWeightUsecase = .init(weightRepository: weightRepository)
+        getRecentWeightUsecase = .init(weightRepository: weightRepository)
+        
+        value = getRecentWeightUsecase.implement()?.value ?? 0 
     }
 }
