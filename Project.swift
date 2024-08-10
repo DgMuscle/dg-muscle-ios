23d6791d0e12d7304789e85049922ace9271533c
신동규 <donggyu9410@gmail.com>
2024-08-10 23:11:03 +0900

    chore: version

--- Project.swift
+++ Project.swift
@@ -3,7 +3,7 @@ import ProjectDescription
 let projectName = "dg-muscle-ios"
 let widgetName = "DgWidget"
 let bundleId = "com.donggyu.dg-muscle-ios"
-let appVersion: String = "2.9.0"
+let appVersion: String = "2.10.0"
 
 enum Layer: String, CaseIterable {
     case Domain
